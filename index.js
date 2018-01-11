e9afa5215cc3d429e5ebdc8d6c9f45c0a90543b7
Felipe <felipearaujosoares@localhost.localdomain>
2018-01-11 12:17:32 -0200

    db access

--- index.js
+++ index.js
@@ -37,3 +37,34 @@ router.post('/sum', function (req, res) {
 }).required(), 'Sum of the input values.')
 .summary('Add up numbers')
 .description('Calculates the sum of an array of number values.');
+
+const db = require('@arangodb').db;
+const errors = require('@arangodb').errors;
+const foxxColl = db._collection('myFoxxCollection');
+const DOC_NOT_FOUND = errors.ERROR_ARANGO_DOCUMENT_NOT_FOUND.code;
+
+router.post('/entries', function (req, res) {
+  const data = req.body;
+  const meta = foxxColl.save(req.body);
+  res.send(Object.assign(data, meta));
+})
+.body(joi.object().required(), 'Entry to store in the collection.')
+.response(joi.object().required(), 'Entry stored in the collection.')
+.summary('Store an entry')
+.description('Stores an entry in the "myFoxxCollection" collection.');
+
+router.get('/entries/:key', function (req, res) {
+  try {
+    const data = foxxColl.document(req.pathParams.key);
+    res.send(data)
+  } catch (e) {
+    if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+      throw e;
+    }
+    res.throw(404, 'The entry does not exist', e);
+  }
+})
+.pathParam('key', joi.string().required(), 'Key of the entry.')
+.response(joi.object().required(), 'Entry stored in the collection.')
+.summary('Retrieve an entry')
+.description('Retrieves an entry from the "myFoxxCollection" collection by key.');
